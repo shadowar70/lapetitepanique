0a7404ad306a01cd95852f7b7badf7f1b080bb73
Léo Le Sommer <llesommer.dev@gmail.com>
2018-11-10 10:48:23 +0100

    Movement correction

--- Assets/Scripts/RandomMovement.cs
+++ Assets/Scripts/RandomMovement.cs
@@ -1,18 +1,46 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class RandomMovement : MonoBehaviour
 {
-	public Vector2 direction = Vector2.zero;
+    public float range = 30.0f;
 
-	void Start ()
+    private NavMeshAgent navMeshAgent = null;
+
+    void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.destination = RandomNavmeshLocation(range);
+    }
+
+    void Update ()
 	{
-		
-	}
-	
-	void Update ()
-	{
-		
-	}
+        // Check if we've reached the destination
+        if (!navMeshAgent.pathPending)
+        {
+            if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+            {
+                if (!navMeshAgent.hasPath || navMeshAgent.velocity.sqrMagnitude == 0f)
+                {
+                    navMeshAgent.SetDestination(RandomNavmeshLocation(range));
+                }
+            }
+        }
+    }
+
+    public Vector3 RandomNavmeshLocation(float radius)
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * radius;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        Vector3 finalPosition = Vector3.zero;
+        if (NavMesh.SamplePosition(randomDirection, out hit, radius, 1))
+        {
+            finalPosition = hit.position;
+        }
+
+        return finalPosition;
+    }
 }
