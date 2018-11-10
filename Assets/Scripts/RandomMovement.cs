c7f5164a332f604dee62525bba0cab67d160a13c
Léo Le Sommer <llesommer.dev@gmail.com>
2018-11-10 09:52:24 +0100

    New .NET framework version used

---
+++ Assets/Scripts/RandomMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMovement : MonoBehaviour
+{
+	public Vector2 direction = Vector2.zero;
+
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		
+	}
+}
