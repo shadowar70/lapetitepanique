fec2bad7ddd7a5d7e6c8ad9f51969688fdd5211c
Léo Le Sommer <llesommer.dev@gmail.com>
2018-11-10 20:55:20 +0100

    Des trucs

---
+++ Assets/Scripts/Audio/PlayOnDestroyed.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayOnDestroyed : MonoBehaviour
+{
+	public string audioSourceTag = null;
+	public AudioClip[] audioClips = new AudioClip[0];
+
+	private AudioSource audioSource;
+
+	void Awake()
+	{
+		audioSource = GameObject.FindGameObjectWithTag(audioSourceTag).GetComponent<AudioSource>();
+	}
+
+	void OnDestroy()
+	{
+		if(!audioSource.isPlaying)
+		{
+			audioSource.PlayOneShot(audioClips[Random.Range(0, audioClips.Length)]);
+		}
+	}
+}
