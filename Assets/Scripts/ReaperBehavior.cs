f3ac6f46da8ca2566c027b07e2a20c6ed9de3dac
shad0war <jeremy.othenin@laposte.net>
2018-11-10 12:18:30 +0100

    add highlight + kill innocent feature on click

---
+++ Assets/Scripts/ReaperBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReaperBehavior : MonoBehaviour {
+
+
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+
+        if (Input.GetButtonDown("Fire1")) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                if (hit.transform.CompareTag("Innocent")){
+                    Destroy(hit.transform.gameObject);
+                }
+                
+
+
+            }
+        }
+
+    }
+
+
+
+}
